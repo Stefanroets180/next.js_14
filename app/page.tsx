22f9749ca7f03ab8e5bbb7941b780cc99547be74
Stefan Roets <stefanroets@Stefans-MacBook-Pro.local>
2024-08-13 14:23:41 +0200

    11.Route Handlers

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 
-export default function Home() {
-throw new Error("somthe went wring ") //just error testing
+export default async function Home() {
+    const data = await fetch ("https://jsonplaceholder.typicode")
+    const todo = await data.json()
+    console.log(todo)
   return (
     <main>
-      <h1>Welcome to Next.js</h1>
+      <h1>{todo.title}</h1>
       <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16}/>
     </main>
   )
