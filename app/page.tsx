7e646371d5fb3f48faf5c6d0fa994cf1ad82d96b
Stefan Roets <stefanroets@Stefans-MacBook-Pro.local>
2024-08-13 13:52:23 +0200

    10.Error Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
 export default function Home() {
+throw new Error("somthe went wring ") //just error testing
   return (
     <main>
       <h1>Welcome to Next.js</h1>
